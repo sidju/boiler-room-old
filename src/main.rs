c750010d737bf4503f6a26a16ea2d33e12651aa4
David Olofsson <contact@sidju.se>
2021-09-13 20:43:41 +0200

    First boilerplate done

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+  let path = req.uri().path();
+  Ok(Response::new(format!("Hello at {}", path).into()))
+}
+
+#[tokio::main]
+async fn main() {
+  // Define the socket to bind to
+  let addr = SocketAddr::from( ([0,0,0,0], 8080) );
+
+  // Define what to do with requests
+  // - A Service is a stateful worker that responds to one request at a time.
+  //   service_fn creates a Service from a FnMut that accepts Request and returns a Response Future.
+  // - A "MakeService" is a Service that creates more Services.
+  //   make_service_fn is essentially the same as service_fn, but requiring that Fn::Return is a Service
+  //   Since we can create that from a closure, we can bind in variables to all created Services
+  let make_service = make_service_fn(
+    |_conn| async move {
+      Ok::<_, Infallible>( service_fn(handle_request) )
+    }
+  );
+
+  // Create and configure the server
+  let server = Server::bind(&addr).serve(make_service);
+
+  // Finally run the server (forever)
+  match server.await {
+    Ok(_) => println!("Server ran successfully"),
+    Err(e) => eprintln!("Error occured: {}", e),
+  }
+}
